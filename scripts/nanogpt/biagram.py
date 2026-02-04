dcf260bf01bc413c3c78b5590dc96c5bf648efca
rahulkolyikkath <rahulkolayikkath@gmail.com>
2026-02-04 13:53:11 +0530

    skeleton for biagram.py

---
+++ scripts/nanogpt/biagram.py
@@ -0,0 +1,33 @@
+""" Script to implement biagram language model"""
+
+# imports
+
+
+# hyperparameters 
+
+
+# read the data 
+
+
+# identify vocabulary
+
+
+# encoder and decoder 
+
+
+
+# train and val split 
+
+
+# get batch 
+
+
+# biagram language model 
+
+
+# training loop 
+
+
+# generate sample text 
+
+
