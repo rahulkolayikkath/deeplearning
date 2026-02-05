27ea382dcbd9f2325d14ebb6c6f739c350a98951
rahulkolyikkath <rahulkolayikkath@gmail.com>
2026-02-05 15:42:06 +0530

    added blocks of transformer and added residual connections

--- scripts/nanogpt/transformer.py
+++ scripts/nanogpt/transformer.py
@@ -15,8 +15,7 @@ eval_iters = 200
 eval_interval = 500 
 device = "cuda" if torch.cuda.is_available() else 'cpu'
 n_embd = 32 # no of embedding dimention
-head_size = 32
-num_heads = 4
+n_heads = 4
 torch.manual_seed(1337)
 # -------------------------------------
 
@@ -71,6 +70,7 @@ def get_batch(split):
 class Head(nn.Module):
     def __init__(self, head_size):
         super().__init__()
+        self.head_size = head_size
         self.key = nn.Linear(n_embd, head_size, bias = False)
         self.query = nn.Linear(n_embd, head_size, bias = False)
         self.value = nn.Linear(n_embd, head_size, bias = False)
@@ -82,7 +82,7 @@ class Head(nn.Module):
         k = self.key(x) #(B, T, head_size)
         q = self.query(x) #(B, T, head_size)
         # compute attention scores ('affinities')
-        wei = q @ k.transpose(-2, -1)* head_size**-0.5 # (B, T, head_size) @ (B, head_size, T) --> (B,T,T)
+        wei = q @ k.transpose(-2, -1)* self.head_size**-0.5 # (B, T, head_size) @ (B, head_size, T) --> (B,T,T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) #(B,T,T)
         wei = F.softmax(wei, dim =-1 ) #(B,T,T)
         # perform weighted aggregation of values
@@ -96,21 +96,41 @@ class MultiheadAttenion(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd) # projection layer back into Residual pathway
 
     def forward(self, x):
-        return torch.cat( [h(x) for h in self.heads], dim = -1) # concatenate in channel dimention
+        out = torch.cat( [h(x) for h in self.heads], dim = -1) # concatenate in channel dimention
+        out = self.proj(out)
+        return out
 
 # class implementation for feed forward network 
 class FeedForward(nn.Module):
-    def __init__(self, fan_in, fan_out):
+    def __init__(self, n_embd):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(fan_in, fan_out),
+            nn.Linear(n_embd, 4*n_embd), # from the transformer paper
             nn.ReLU(),
+            nn.Linear(4*n_embd, n_embd) # projection layer back into Residual pathway
         )
     
     def forward(self, x):
         return self.net(x)
+    
+# class implementation for tranformer block
+class Block(nn.Module):
+    """ Transformer block: communication and then computation"""
+    def __init__(self,n_embd, n_heads):
+        # embedding_dimension and no of heads we like
+        super().__init__()
+        head_size  = n_embd // n_heads
+        self.sa_heads = MultiheadAttenion(n_heads, head_size)
+        self.ffwd = FeedForward(n_embd)
+
+    def forward(self, x):
+        # residual connections--> x = x + computation
+        x = x + self.sa_heads(x) # (B, T, head_size) # here head_size = n_embd
+        x = x + self.ffwd(x) # (B, T, n_embd)
+        return x
     
 # transformer language model, biagram sampling
 class TransformerLanguageModel(nn.Module):
@@ -120,9 +140,15 @@ class TransformerLanguageModel(nn.Module):
         # nn.Embedding -> A simple lookup table that stores embeddings of a fixed dictionary and size.
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.postion_embedding_table = nn.Embedding(block_size, n_embd)
-        #self.sa_head = Head(n_embd)
-        self.sa_heads = MultiheadAttenion(num_heads, n_embd//num_heads) # 4 heads of 8head size 
-        self.ffwd = FeedForward(n_embd, n_embd) # add computation after attention
+        #self.sa_head = Head(n_embd). --> when single head attention 
+        #self.sa_heads = MultiheadAttenion(num_heads, n_embd//num_heads) # 4 heads of 8head size --> when single multihead attention
+        #self.ffwd = FeedForward(n_embd, n_embd) # add computation after attention --> when single multihead attention
+        self.blocks = nn.Sequential(
+            Block(n_embd, n_heads),
+            Block(n_embd, n_heads),
+            Block(n_embd, n_heads),
+            Block(n_embd, n_heads),
+        )
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx,targets=None):
@@ -131,8 +157,10 @@ class TransformerLanguageModel(nn.Module):
         token_embeddings = self.token_embedding_table(idx) # (B,T,n_embd)
         position_embeddings = self.postion_embedding_table(torch.arange(T, device=device)) # (T, n_embd)
         x = token_embeddings + position_embeddings # (B, T, n_embd) + (B, T, n_embd) -> (B,T,n_embd)
-        x = self.sa_heads(x) # (B, T, head_size) # here head_size = n_embd
-        x = self.ffwd(x) # (B, T, n_embd)
+        #x = self.sa_head(x) 
+        #x = self.sa_heads(x) # (B, T, head_size) # here head_size = n_embd
+        #x = self.ffwd(x) # (B, T, n_embd)
+        x = self.blocks(x)
         logits = self.lm_head(x) # (B, T, vocab_size)
 
         if targets is None:
